499412eb728b9d90fe0ee331e68a701eb93a2643
surrealcristian <surrealcristian@gmail.com>
2015-12-08 23:09:55 -0300

    more oop, particles, new sprites

--- js/game.js
+++ js/game.js
@@ -1,231 +1,349 @@
 ; (function () {
 
-  var WIDTH = 500;
-  var HEIGHT = 600;
+// --------------------------------------------------------------------------
+// Custom game objects
+// --------------------------------------------------------------------------
 
-  var keys = {};
-  var colors = {grey: '#777777'};
+Game = function() {
+  var w = 500;
+  var h = 600;
 
-  var player;
-  var blackEnemies;
-  var cursors;
-  var bullets;
-  var fireButton;
-  var bulletTimer = 0;
+  Phaser.Game.call(this, w, h, Phaser.AUTO, 'game');
+};
 
-  var fireBullet = function() {
-    if (game.time.now < bulletTimer) {
-      return;
-    }
+Game.prototype = Object.create(Phaser.Game.prototype);
+Game.prototype.constructor = Game;
 
-    var BULLET_SPEED = 400;
-    var BULLET_SPACING = 250;
+Player = function(g) {
+  var w = 25;
+  var h = 25;
+  var x = g.width / 2;
+  var y = g.height * .9;
 
-    var bullet = bullets.getFirstExists(false);
+  //var bmd = g.add.bitmapData(w, h);
+  //bmd.ctx.beginPath();
+  //bmd.ctx.rect(0, 0, w, h);
+  //bmd.ctx.fillStyle = '#ffffff';
+  //bmd.ctx.fill();
 
-    if (!bullet) {
-      return;
-    }
+  Phaser.Sprite.call(this, g, x, y, 'player_red');
 
-    bullet.reset(player.x, player.y - player.height * 0.7);
-    bullet.body.velocity.y = -BULLET_SPEED;
+  this.width = w;
+  this.height = h;
 
-    bulletTimer = game.time.now + BULLET_SPACING;
-  };
+  this.bulletTimer = 0;
+  this.bulletSpeed = 400;
+  this.bulletSpacing = 100;
 
-  var launchBlackEnemy = function() {
-    var MIN_ENEMY_SPACING = 300;
-    var MAX_ENEMY_SPACING = 3000;
-    var ENEMY_SPEED = 100;
+  this.anchor.setTo(.5, .5)
 
-    game.time.events.add(
-      game.rnd.integerInRange(MIN_ENEMY_SPACING, MAX_ENEMY_SPACING),
-      launchBlackEnemy
-    );
+  g.physics.arcade.enable(this);
 
-    var enemy = blackEnemies.getFirstExists(false);
+  this.body.collideWorldBounds = true;
+};
 
-    if (!enemy) {
-      return;
-    }
+Player.prototype = Object.create(Phaser.Sprite.prototype);
+Player.prototype.constructor = Player;
 
-    var x = game.rnd.integerInRange(
-      0 + enemy.width / 2,
-      game.width - enemy.width / 2
-    );
-    var y = 0 - enemy.height / 2;
-
-    enemy.reset(x, y);
-    enemy.body.velocity.y = ENEMY_SPEED;
-  };
-
-  var states = {
-    init: {
-      preload: function() {
-        game.scale.pageAlignHorizontally = true;
-        game.scale.pageAlignVertically = true;
-        game.scale.refresh();
-
-        game.state.start('title');
-      }
-    },
-
-    title: {
-      preload: function() {
-        keys.enter = game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
-        keys.delete = game.input.keyboard.addKey(Phaser.Keyboard.DELETE);
-
-        var style = {fill: colors.grey, align: "center"};
-        var text = game.add.text(
-          game.world.centerX, game.world.centerY,
-          'Press ENTER to start', style
-        );
-
-        text.anchor.setTo(0.5, 0.5);
-      },
-
-      update: function() {
-        if (keys.enter.isDown) {
-          game.state.start('game');
-        }
-      }
-    },
-
-    game: {
-      preload: function() {
-        game.load.image('ship', 'assets/playerShip3_blue.png');
-        game.load.image('blackEnemy', 'assets/enemyBlack3.png');
-        game.load.image('blueBullet', 'assets/laserBlue03.png');
-        game.load.image('redBullet', 'assets/laserRed03.png');
-      },
-
-      create: function() {
-        cursors = game.input.keyboard.createCursorKeys();
-        fireButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-
-        var createPlayer = function() {
-          var PLAYER_WIDTH = 50;
-          var PLAYER_HEIGHT = 50;
-
-          player = game.add.sprite(game.width / 2, game.height * .9, 'ship');
-          player.anchor.setTo(0.5, 0.5);
-          player.width = PLAYER_WIDTH;
-          player.height = PLAYER_HEIGHT;
-
-          game.physics.enable(player, Phaser.Physics.ARCADE);
-
-          player.body.collideWorldBounds = true;
-
-          player.body.setSize(player.width * 1, player.height * 1);
-        };
-
-        var createPlayerBullets = function() {
-          bullets = game.add.group();
-          bullets.enableBody = true;
-          bullets.physicsBodyType = Phaser.Physics.ARCADE;
-          bullets.createMultiple(30, 'blueBullet');
-          bullets.setAll('anchor.x', 0.5);
-          bullets.setAll('anchor.y', 0.5);
-          bullets.setAll('outOfBoundsKill', true);
-          bullets.setAll('checkWorldBounds', true);
-        };
-
-        var createBlackEnemies = function() {
-          blackEnemies = game.add.group();
-          blackEnemies.enableBody = true;
-          blackEnemies.physicsBodyType = Phaser.Physics.ARCADE;
-          blackEnemies.createMultiple(5, 'blackEnemy');
-          blackEnemies.setAll('anchor.x', 0.5);
-          blackEnemies.setAll('anchor.y', 0.5);
-          blackEnemies.setAll('scale.x', 0.5);
-          blackEnemies.setAll('scale.y', 0.5);
-          blackEnemies.setAll('outOfBoundsKill', true);
-          blackEnemies.setAll('checkWorldBounds', true);
-
-          blackEnemies.forEach(function(enemy) {
-            enemy.body.setSize(enemy.width, enemy.height * 3/4);
-          });
-        };
-
-        createPlayer();
-        createPlayerBullets();
-        createBlackEnemies();
-
-        launchBlackEnemy();
-      },
-
-      update: function() {
-        var onPressCursors = function() {
-          var PLAYER_VELOCITY = 300;
-          player.body.velocity.setTo(0, 0);
-
-          if (cursors.left.isDown)  { player.body.velocity.x += -PLAYER_VELOCITY; }
-          if (cursors.right.isDown) { player.body.velocity.x += PLAYER_VELOCITY; }
-          if (cursors.up.isDown)    { player.body.velocity.y += -PLAYER_VELOCITY; }
-          if (cursors.down.isDown)  { player.body.velocity.y += PLAYER_VELOCITY; }
-        };
-
-        var onPressFire = function() {
-          if (fireButton.isDown) {
-            fireBullet();
-          }
-        };
-
-        var onPressDelete = function() {
-          if (keys.delete.isDown) {
-            game.state.start('gameOver');
-          }
-        };
-
-        var checkCollisions = function() {
-          var shipCollide = function(player, enemy) {
-            enemy.kill();
-          };
-
-          game.physics.arcade.overlap(player, blackEnemies, shipCollide, null, this);
-        };
-
-        onPressCursors();
-        onPressFire();
-        onPressDelete();
-        checkCollisions();
-      },
-
-      render: function() {
-        game.debug.body(player);
-
-        for (var i = 0; i < blackEnemies.length; i++) {
-          game.debug.body(blackEnemies.children[i]);
-        }
-      }
-    },
-
-    gameOver: {
-      preload: function() {
-        var style = {fill: colors.grey, align: "center"};
-        var text = game.add.text(
-            game.world.centerX, game.world.centerY,
-            'GAME OVER\nPress ENTER to continue', style
-        );
-
-        text.anchor.setTo(0.5, 0.5);
-      },
-
-      update: function() {
-        if (keys.enter.isDown) {
-          game.state.start('game');
-        }
-      }
-    }
+Player.prototype.fire = function() {
+  if (g.time.now < this.bulletTimer) {
+    return;
   }
 
-  var game = new Phaser.Game(WIDTH, HEIGHT, Phaser.AUTO, 'game');
+  var bullet = this.bullets.getFirstExists(false);
 
-  game.state.add('init', states.init);
-  game.state.add('title', states.title);
-  game.state.add('game', states.game);
-  game.state.add('gameOver', states.gameOver);
+  if (!bullet) {
+    return;
+  }
 
-  game.state.start('init');
+  bullet.reset(this.x, this.y - this.height * .5);
+  bullet.body.velocity.y = -this.bulletSpeed;
+
+  this.bulletTimer = g.time.now + this.bulletSpacing;
+};
+
+PlayerBulletGroup = function(g) {
+  Phaser.Group.call(this, g);
+
+  var w = 5;
+  var h = 20;
+
+  //var bmd = g.add.bitmapData(w, h);
+  //bmd.ctx.beginPath();
+  //bmd.ctx.rect(0, 0, w, h);
+  //bmd.ctx.fillStyle = '#ffffff';
+  //bmd.ctx.fill();
+
+  this.createMultiple(30, 'laser_red');
+
+  this.setAll('width', w);
+  this.setAll('height', h);
+  this.setAll('anchor.x', 0.5);
+  this.setAll('anchor.y', 0.5);
+  this.setAll('checkWorldBounds', true);
+  this.setAll('outOfBoundsKill', true);
+
+  g.physics.arcade.enable(this);
+};
+
+PlayerBulletGroup.prototype = Object.create(Phaser.Group.prototype);
+PlayerBulletGroup.prototype.constructor = PlayerBulletGroup;
+
+EnemyGroup = function(g) {
+  Phaser.Group.call(this, g);
+
+  var w = 25;
+  var h = 25
+
+  //var bmd = g.add.bitmapData(w, h);
+  //bmd.ctx.beginPath();
+  //bmd.ctx.rect(0, 0, w, h);
+  //bmd.ctx.fillStyle = '#aaaaaa';
+  //bmd.ctx.fill();
+
+  this.createMultiple(5, 'ufo_green');
+
+  this.setAll('width', w);
+  this.setAll('height', h);
+  this.setAll('anchor.x', 0.5);
+  this.setAll('anchor.y', 0.5);
+  this.setAll('checkWorldBounds', true);
+  this.setAll('outOfBoundsKill', true);
+
+  g.physics.arcade.enable(this);
+};
+
+EnemyGroup.prototype = Object.create(Phaser.Group.prototype);
+EnemyGroup.prototype.constructor = EnemyGroup;
+
+// --------------------------------------------------------------------------
+// Everything else
+// --------------------------------------------------------------------------
+
+// Returns a random integer between min (included) and max (excluded)
+// Using Math.round() will give you a non-uniform distribution!
+var randInt = function(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
+var keys = {};
+var colors = {grey: '#777777'};
+
+// ----------------------------------------------------------------------------
+// States
+// ----------------------------------------------------------------------------
+
+var InitState = function() {};
+
+InitState.prototype = {
+  init: function() {},
+
+  preload: function() {
+      g.scale.pageAlignHorizontally = true;
+      g.scale.pageAlignVertically = true;
+      g.scale.refresh();
+
+      g.state.start('title');
+  }
+};
+
+var TitleState = function() {};
+
+TitleState.prototype = {
+  preload: function() {
+    keys.enter = g.input.keyboard.addKey(Phaser.Keyboard.ENTER);
+    keys.delete = g.input.keyboard.addKey(Phaser.Keyboard.DELETE);
+
+    var style = {fill: colors.grey, align: "center"};
+    var text = g.add.text(
+      g.world.centerX, g.world.centerY,
+      'Press ENTER to start', style
+    );
+
+    text.anchor.setTo(0.5, 0.5);
+  },
+
+  update: function() {
+    if (keys.enter.isDown) {
+      g.state.start('game');
+    }
+  }
+};
+
+var GameState = function() {
+  this.fireButton = null;
+  this.p = null;
+  this.enemies = null;
+  this.arcade = null;
+  this.emitter = null;
+  this.dfc = 10; // distance from center
+};
+
+GameState.prototype = {
+  launchEnemy: function() {
+    var minSpacing = 300;
+    var maxSpacing = 3000;
+
+    g.time.events.add(
+      randInt(minSpacing, maxSpacing),
+      this.launchEnemy, this
+    );
+
+    var e = this.enemies.getFirstExists(false);
+
+    if (!e) {
+      return;
+    }
+
+    var x = randInt(0 + e.width / 2, g.width - e.width / 2);
+    var y = 0 - e.height / 2;
+    var speed = 100;
+
+    e.reset(x, y);
+    e.body.velocity.y = speed;
+    e.health = 3;
+    e.immovable = true;
+  },
+
+  followMouse: function() {
+    var dfc = 10; //distance from center
+
+    if ((this.p.x <= g.input.x + this.dfc && this.p.x >= g.input.x - this.dfc)
+        && (this.p.y <= g.input.y + this.dfc && this.p.y >= g.input.y - this.dfc)) {
+      this.p.body.velocity.setTo(0, 0);
+      return;
+    }
+
+    this.arcade.moveToPointer(this.p, 400);
+  },
+
+  checkCollisions: function() {
+    var cb = function(p, e) {
+      p.kill();
+      e.kill();
+      g.state.start('gameOver');
+    };
+    this.arcade.collide(this.p, this.enemies, cb, null, this);
+
+    var cb = function(b, e) {
+      this.particleBurst(e.x, e.y);
+      b.kill();
+      e.damage(1);
+    };
+    this.arcade.overlap(this.p.bullets, this.enemies, cb, null, this);
+  },
+
+  particleBurst: function(x, y) {
+    this.emitter.x = x;
+    this.emitter.y = y;
+
+    this.emitter.start(true, 500, null, 5);
+  },
+
+  preload: function() {
+    g.renderer.renderSession.roundPixels = true;
+    this.physics.startSystem(Phaser.Physics.ARCADE);
+    this.arcade = this.game.physics.arcade;
+    g.time.advancedTiming = true;
+
+    g.load.image('player_red', 'img/player.png');
+    g.load.image('ufo_green', 'img/ufo_green.png');
+    g.load.image('laser_red', 'img/laser_red.png');
+    g.load.image('explosion_particle', 'img/explosion_particle.png');
+  },
+
+  create: function() {
+    this.fireButton = g.input.keyboard.addKey(Phaser.Keyboard.Z);
+
+    this.p = new Player(g);
+    g.add.existing(this.p);
+
+    this.p.bullets = new PlayerBulletGroup(g);
+
+    this.enemies = new EnemyGroup(g);
+
+    this.launchEnemy();
+
+    this.emitter = g.add.emitter(0, 0, 100);
+
+    var w = 5;
+    var h = 5;
+
+    var bmd = g.add.bitmapData(w, h);
+    bmd.ctx.beginPath();
+    bmd.ctx.rect(0, 0, w, h);
+    bmd.ctx.fillStyle = '#ffffff';
+    bmd.ctx.fill();
+
+    this.emitter.makeParticles(bmd);
+
+    this.emitter.gravity = 0;
+    this.emitter.setAlpha(.5, .5);
+    this.emitter.setXSpeed(-2000, 2000);
+    this.emitter.setYSpeed(-2000, 2000);
+  },
+
+  update: function() {
+    this.followMouse();
+
+    if (this.fireButton.isDown) {
+      this.p.fire();
+    }
+
+    if (keys.delete.isDown) {
+      g.state.start('gameOver');
+    }
+
+    this.checkCollisions();
+  },
+
+  render: function() {
+    //uncomment to debug physics
+    /*
+    g.debug.body(this.p);
+
+    var cb = function(enemy) {
+      g.debug.body(enemy);
+    }
+
+    this.enemies.forEachAlive(cb, this);
+
+    g.debug.text(g.time.fps || '--', 2, 14, '#00ff00');
+    */
+  }
+};
+
+var GameOverState = function() {};
+
+GameOverState.prototype = {
+  preload: function() {
+    var style = {fill: colors.grey, align: "center"};
+    var text = g.add.text(
+      g.world.centerX, g.world.centerY,
+      'GAME OVER\nPress ENTER to continue', style
+    );
+
+    text.anchor.setTo(0.5, 0.5);
+  },
+
+  update: function() {
+    if (keys.enter.isDown) {
+      g.state.start('game');
+    }
+  }
+};
+
+// ----------------------------------------------------------------------------
+// Game creation and states loading
+// ----------------------------------------------------------------------------
+
+var g = new Game();
+
+g.state.add('init', InitState);
+g.state.add('title', TitleState);
+g.state.add('game', GameState);
+g.state.add('gameOver', GameOverState);
+
+g.state.start('init');
 
 }());
